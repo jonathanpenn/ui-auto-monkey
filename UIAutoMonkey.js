6890bb481f4fc80bd6e7a35aaabe80b07dc69c2a
Jonathan Penn <jonathan@navel-labs.com>
2012-11-02 23:42:59 -0400

    Converted spaces to tabs for the main use case
    Most people using this are pasting the code directly into the
    Instruments script editor and tweaking from there. I usually like my
    JavaScript indented 2 spaces, but since the Instruments editor uses
    tabs, I decided that converting to that is best.
    
    I put a modeline at the bottom for vim.

--- UIAutoMonkey.js
+++ UIAutoMonkey.js
@@ -20,209 +20,212 @@
 
 UIAutoMonkey = {
 
-  config: {
-    numberOfEvents: 1000,
-    delayBetweenEvents: 0.05,    // In seconds
+	config: {
+		numberOfEvents: 1000,
+		delayBetweenEvents: 0.05,    // In seconds
 
-    // Events are triggered based on the relative weights here. The event with
-    // this highest number gets triggered the most.
-    //
-    // If you want to add your own "events", check out the event method
-    // definitions below.
-    eventWeights: {
-      tap: 500,
-      drag: 1,
-      flick: 1,
-      orientation: 1,
-      clickVolumeUp: 1,
-      clickVolumeDown: 1,
-      lock: 1
-    },
+		// Events are triggered based on the relative weights here. The event
+		// with this highest number gets triggered the most.
+		//
+		// If you want to add your own "events", check out the event method
+		// definitions below.
+		eventWeights: {
+			tap: 500,
+			drag: 1,
+			flick: 1,
+			orientation: 1,
+			clickVolumeUp: 1,
+			clickVolumeDown: 1,
+			lock: 1
+		},
 
-    // Probability that touch events will have these different properties
-    touchProbability: {
-      multipleTaps: 0.05,
-      multipleTouches: 0.05,
-      longPress: 0.05
-    }
-  },
+		// Probability that touch events will have these different properties
+		touchProbability: {
+			multipleTaps: 0.05,
+			multipleTouches: 0.05,
+			longPress: 0.05
+		}
+	},
 
-  // --- --- --- ---
-  // Event Methods
-  //
-  // Any event probability in the hash above corresponds to a related event method
-  // below. So, a "tap" probability will trigger a "tapEvent" method.
-  //
-  // If you want to add your own events, just add a probability to the hash
-  // above and then add a corresponding method below. Boom!
-  //
-  // Each event method can call any other method on this UIAutoMonkey object.
-  // All the methods at the end are helpers at your disposal and feel free to
-  // add your own.
+	// --- --- --- ---
+	// Event Methods
+	//
+	// Any event probability in the hash above corresponds to a related event
+	// method below. So, a "tap" probability will trigger a "tapEvent" method.
+	//
+	// If you want to add your own events, just add a probability to the hash
+	// above and then add a corresponding method below. Boom!
+	//
+	// Each event method can call any other method on this UIAutoMonkey object.
+	// All the methods at the end are helpers at your disposal and feel free to
+	// add your own.
 
-  tapEvent: function() {
-    this.target().tapWithOptions(
-      { x: this.randomX(), y: this.randomY() },
-      {
-        tapCount: this.randomTapCount(),
-        touchCount: this.randomTouchCount(),
-        duration: this.randomTapDuration()
-      }
-    );
-  },
+	tapEvent: function() {
+		this.target().tapWithOptions(
+			{ x: this.randomX(), y: this.randomY() },
+			{
+				tapCount: this.randomTapCount(),
+				touchCount: this.randomTouchCount(),
+				duration: this.randomTapDuration()
+			}
+		);
+	},
 
-  dragEvent: function() {
-    this.target().dragFromToForDuration(
-      { x: this.randomX(), y: this.randomY() },
-      { x: this.randomX(), y: this.randomY() },
-      0.5
-    );
-  },
+	dragEvent: function() {
+		this.target().dragFromToForDuration(
+			{ x: this.randomX(), y: this.randomY() },
+			{ x: this.randomX(), y: this.randomY() },
+			0.5
+		);
+	},
 
-  flickEvent: function() {
-    this.target().flickFromTo(
-      { x: this.randomX(), y: this.randomY() },
-      { x: this.randomX(), y: this.randomY() }
-    );
-  },
+	flickEvent: function() {
+		this.target().flickFromTo(
+			{ x: this.randomX(), y: this.randomY() },
+			{ x: this.randomX(), y: this.randomY() }
+		);
+	},
 
-  orientationEvent: function() {
-    var orientations = [
-      UIA_DEVICE_ORIENTATION_PORTRAIT,
-      UIA_DEVICE_ORIENTATION_PORTRAIT_UPSIDEDOWN,
-      UIA_DEVICE_ORIENTATION_LANDSCAPELEFT,
-      UIA_DEVICE_ORIENTATION_LANDSCAPERIGHT
-    ];
+	orientationEvent: function() {
+		var orientations = [
+			UIA_DEVICE_ORIENTATION_PORTRAIT,
+			UIA_DEVICE_ORIENTATION_PORTRAIT_UPSIDEDOWN,
+			UIA_DEVICE_ORIENTATION_LANDSCAPELEFT,
+			UIA_DEVICE_ORIENTATION_LANDSCAPERIGHT
+		];
 
-    var i = Math.floor(Math.random() * 10) % orientations.length;
-    var newOrientation = orientations[i];
-    this.target().setDeviceOrientation(newOrientation);
-    this.delay(0.9);
-  },
+		var i = Math.floor(Math.random() * 10) % orientations.length;
+		var newOrientation = orientations[i];
+		this.target().setDeviceOrientation(newOrientation);
+		this.delay(0.9);
+	},
 
-  clickVolumeUpEvent: function() {
-    this.target().clickVolumeUp();
-  },
+	clickVolumeUpEvent: function() {
+		this.target().clickVolumeUp();
+	},
 
-  clickVolumeDownEvent: function() {
-    this.target().clickVolumeUp();
-  },
+	clickVolumeDownEvent: function() {
+		this.target().clickVolumeUp();
+	},
 
-  lockEvent: function() {
-    this.target().lockForDuration(Math.random() * 3);
-  },
+	lockEvent: function() {
+		this.target().lockForDuration(Math.random() * 3);
+	},
 
 
-  // --- --- --- ---
-  // Helper methods
-  //
+	// --- --- --- ---
+	// Helper methods
+	//
 
-  RELEASE_THE_MONKEY: function() {
-    // Called at the bottom of this script to, you know...
-    //
-    // RELEASE THE MONKEY!
+	RELEASE_THE_MONKEY: function() {
+		// Called at the bottom of this script to, you know...
+		//
+		// RELEASE THE MONKEY!
 
-    for(var i = 0; i < this.config.numberOfEvents; i++) {
-      this.triggerRandomEvent();
-      this.delay();
-    }
-  },
+		for(var i = 0; i < this.config.numberOfEvents; i++) {
+			this.triggerRandomEvent();
+			this.delay();
+		}
+	},
 
-  triggerRandomEvent: function() {
-    var name = this.chooseEventName();
-    // Find the event method based on the name of the event
-    var event = this[name + "Event"];
-    event.apply(this);
-  },
+	triggerRandomEvent: function() {
+		var name = this.chooseEventName();
+		// Find the event method based on the name of the event
+		var event = this[name + "Event"];
+		event.apply(this);
+	},
 
-  target: function() {
-    // Return the local target.
-    return UIATarget.localTarget();
-  },
+	target: function() {
+		// Return the local target.
+		return UIATarget.localTarget();
+	},
 
-  delay: function(seconds) {
-    // Delay the target by `seconds` (can be a fraction)
-    // Defaults to setting in configuration
-    seconds = seconds || this.config.delayBetweenEvents;
-    this.target().delay(seconds);
-  },
+	delay: function(seconds) {
+		// Delay the target by `seconds` (can be a fraction)
+		// Defaults to setting in configuration
+		seconds = seconds || this.config.delayBetweenEvents;
+		this.target().delay(seconds);
+	},
 
-  chooseEventName: function() {
-    // Randomly chooses an event name from the `eventsWeight` dictionary
-    // based on the given weights.
-    var calculatedEventWeights = [];
-    var totalWeight = 0;
-    var events = this.config.eventWeights;
-    for (event in events) {
-      if (events.hasOwnProperty(event)) {
-        calculatedEventWeights.push({
-          weight: events[event]+totalWeight,
-          event: event
-        });
-        totalWeight += events[event];
-      }
-    }
+	chooseEventName: function() {
+		// Randomly chooses an event name from the `eventsWeight` dictionary
+		// based on the given weights.
+		var calculatedEventWeights = [];
+		var totalWeight = 0;
+		var events = this.config.eventWeights;
+		for (event in events) {
+			if (events.hasOwnProperty(event)) {
+				calculatedEventWeights.push({
+					weight: events[event]+totalWeight,
+					event: event
+				});
+				totalWeight += events[event];
+			}
+		}
 
-    var chosenWeight = Math.random() * 1000 % totalWeight;
+		var chosenWeight = Math.random() * 1000 % totalWeight;
 
-    for (var i = 0; i < calculatedEventWeights.length; i++) {
-      if (chosenWeight < calculatedEventWeights[i].weight) {
-        return calculatedEventWeights[i].event;
-      }
-    }
+		for (var i = 0; i < calculatedEventWeights.length; i++) {
+			if (chosenWeight < calculatedEventWeights[i].weight) {
+				return calculatedEventWeights[i].event;
+			}
+		}
 
-    throw "No even was chosen!";
-  },
+		throw "No even was chosen!";
+	},
 
-  screenWidth: function() {
-    // Need to adjust by one to stay within rectangle
-    return this.target().rect().size.width - 1;
-  },
+	screenWidth: function() {
+		// Need to adjust by one to stay within rectangle
+		return this.target().rect().size.width - 1;
+	},
 
-  screenHeight: function() {
-    // Need to adjust by one to stay within rectangle
-    return this.target().rect().size.height - 1;
-  },
+	screenHeight: function() {
+		// Need to adjust by one to stay within rectangle
+		return this.target().rect().size.height - 1;
+	},
 
-  randomX: function() {
-    // Returns a random X coordinate within the screen rectangle
-    return Math.floor(Math.random() * 10000) % this.screenWidth();
-  },
+	randomX: function() {
+		// Returns a random X coordinate within the screen rectangle
+		return Math.floor(Math.random() * 10000) % this.screenWidth();
+	},
 
-  randomY: function() {
-    // Returns a random Y coordinate within the screen rectangle
-    return Math.floor(Math.random() * 10000) % this.screenHeight();
-  },
+	randomY: function() {
+		// Returns a random Y coordinate within the screen rectangle
+		return Math.floor(Math.random() * 10000) % this.screenHeight();
+	},
 
-  randomTapCount: function() {
-    // Calculates a tap count for tap events based on touch probabilities
-    if (this.config.touchProbability.multipleTaps > Math.random()) {
-      return Math.floor(Math.random() * 10) % 3 + 1;
-    }
-    else return 1;
-  },
+	randomTapCount: function() {
+		// Calculates a tap count for tap events based on touch probabilities
+		if (this.config.touchProbability.multipleTaps > Math.random()) {
+			return Math.floor(Math.random() * 10) % 3 + 1;
+		}
+		else return 1;
+	},
 
-  randomTouchCount: function() {
-    // Calculates a touch count for tap events based on touch probabilities
-    if (this.config.touchProbability.multipleTouches > Math.random()) {
-      return Math.floor(Math.random() * 10) % 3 + 1;
-    }
-    else return 1;
-  },
+	randomTouchCount: function() {
+		// Calculates a touch count for tap events based on touch probabilities
+		if (this.config.touchProbability.multipleTouches > Math.random()) {
+			return Math.floor(Math.random() * 10) % 3 + 1;
+		}
+		else return 1;
+	},
 
-  randomTapDuration: function() {
-    // Calculates whether or not a tap should be a long press based on
-    // touch probabilities
-    if (this.config.touchProbability.longPress > Math.random()) {
-      return 0.5;
-    }
-    else return 0;
-  },
+	randomTapDuration: function() {
+		// Calculates whether or not a tap should be a long press based on
+		// touch probabilities
+		if (this.config.touchProbability.longPress > Math.random()) {
+			return 0.5;
+		}
+		else return 0;
+	},
 
-  randomRadians: function() {
-    // Returns a random radian value
-    return Math.random() * 10 % (3.14159 * 2);
-  }
+	randomRadians: function() {
+		// Returns a random radian value
+		return Math.random() * 10 % (3.14159 * 2);
+	}
 };
 
 UIAutoMonkey.RELEASE_THE_MONKEY();
+
+/* Instruments uses 4-wide tab stops. */
+/* vim: set tabstop=4 shiftwidth=4 softtabstop=4 copyindent noexpandtab: */
