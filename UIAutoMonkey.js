a35ad68c631535be336be529b464496c57dd9caf
Martin Gratzer <mgratzer@gmail.com>
2012-12-05 09:08:41 +0100

    added pinch events

--- UIAutoMonkey.js
+++ UIAutoMonkey.js
@@ -37,6 +37,8 @@ UIAutoMonkey = {
 			clickVolumeUp: 1,
 			clickVolumeDown: 1,
 			lock: 1,
+			pinchClose: 10,
+			pinchOpen: 10,
 			shake: 1
 		},
 
@@ -121,6 +123,22 @@ UIAutoMonkey = {
 
 	lockEvent: function() {
 		this.target().lockForDuration(Math.random() * 3);
+	},
+
+	pinchCloseEvent: function () {
+		this.target().pinchCloseFromToForDuration(
+			{ x: this.randomX(), y: this.randomY() },
+			{ x: this.randomX(), y: this.randomY() },
+			0.5
+		);
+	},
+
+	pinchOpenEvent: function () {
+		this.target().pinchOpenFromToForDuration(
+			{ x: this.randomX(), y: this.randomY() },
+			{ x: this.randomX(), y: this.randomY() },
+			0.5
+		);
 	},
 
 	shakeEvent: function() {
