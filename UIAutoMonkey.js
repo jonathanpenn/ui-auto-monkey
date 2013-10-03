537adca6620b3e1b013c3607f418f1889654c3dc
Olivier Halligon <olivier.halligon@niji.fr>
2013-10-03 18:58:45 +0200

    Converted to generic independent object to be easily imported and configured

--- UIAutoMonkey.js
+++ UIAutoMonkey.js
@@ -20,9 +20,9 @@
 
 "use strict";
 
-var UIAutoMonkey = {
-
-	config: {
+function UIAutoMonkey() {
+		
+	this.config = {
 		numberOfEvents: 1000,
 		delayBetweenEvents: 0.05,    // In seconds
 
@@ -64,22 +64,25 @@ var UIAutoMonkey = {
 		}
 		*/
 
-	},
+	};
+	
+}
 
-	// --- --- --- ---
-	// Event Methods
-	//
-	// Any event probability in the hash above corresponds to a related event
-	// method below. So, a "tap" probability will trigger a "tapEvent" method.
-	//
-	// If you want to add your own events, just add a probability to the hash
-	// above and then add a corresponding method below. Boom!
-	//
-	// Each event method can call any other method on this UIAutoMonkey object.
-	// All the methods at the end are helpers at your disposal and feel free to
-	// add your own.
+// --- --- --- ---
+// Event Methods
+//
+// Any event probability in the hash above corresponds to a related event
+// method below. So, a "tap" probability will trigger a "tap" method.
+//
+// If you want to add your own events, just add a probability to the hash
+// above and then add a corresponding method below. Boom!
+//
+// Each event method can call any other method on this UIAutoMonkey object.
+// All the methods at the end are helpers at your disposal and feel free to
+// add your own.
 
-	tapEvent: function() {
+UIAutoMonkey.prototype.allEvents = {
+	tap: function() {
 		this.target().tapWithOptions(
 			{ x: this.randomX(), y: this.randomY() },
 			{
@@ -90,7 +93,7 @@ var UIAutoMonkey = {
 		);
 	},
 
-	dragEvent: function() {
+	drag: function() {
 		this.target().dragFromToForDuration(
 			{ x: this.randomX(), y: this.randomY() },
 			{ x: this.randomX(), y: this.randomY() },
@@ -98,14 +101,14 @@ var UIAutoMonkey = {
 		);
 	},
 
-	flickEvent: function() {
+	flick: function() {
 		this.target().flickFromTo(
 			{ x: this.randomX(), y: this.randomY() },
 			{ x: this.randomX(), y: this.randomY() }
 		);
 	},
 
-	orientationEvent: function() {
+	orientation: function() {
 		var orientations = [
 			UIA_DEVICE_ORIENTATION_PORTRAIT,
 			UIA_DEVICE_ORIENTATION_PORTRAIT_UPSIDEDOWN,
@@ -119,19 +122,19 @@ var UIAutoMonkey = {
 		this.delay(0.9);
 	},
 
-	clickVolumeUpEvent: function() {
+	clickVolumeUp: function() {
 		this.target().clickVolumeUp();
 	},
 
-	clickVolumeDownEvent: function() {
+	clickVolumeDown: function() {
 		this.target().clickVolumeUp();
 	},
 
-	lockEvent: function() {
+	lock: function() {
 		this.target().lockForDuration(Math.random() * 3);
 	},
 
-	pinchCloseEvent: function () {
+	pinchClose: function () {
 		this.target().pinchCloseFromToForDuration(
 			{ x: this.randomX(), y: this.randomY() },
 			{ x: this.randomX(), y: this.randomY() },
@@ -139,7 +142,7 @@ var UIAutoMonkey = {
 		);
 	},
 
-	pinchOpenEvent: function () {
+	pinchOpen: function () {
 		this.target().pinchOpenFromToForDuration(
 			{ x: this.randomX(), y: this.randomY() },
 			{ x: this.randomX(), y: this.randomY() },
@@ -147,157 +150,184 @@ var UIAutoMonkey = {
 		);
 	},
 
-	shakeEvent: function() {
+	shake: function() {
 		this.target().shake();
-	},
-
-	// --- --- --- ---
-	// Helper methods
-	//
-
-	RELEASE_THE_MONKEY: function() {
-		// Called at the bottom of this script to, you know...
-		//
-		// RELEASE THE MONKEY!
-
-		for(var i = 0; i < this.config.numberOfEvents; i++) {
-			this.triggerRandomEvent();
-			if (this.config.screenshotInterval) this.takeScreenShotIfItIsTime();
-			this.delay();
-		}
-	},
-
-	triggerRandomEvent: function() {
-		var name = this.chooseEventName();
-		// Find the event method based on the name of the event
-		var event = this[name + "Event"];
-		event.apply(this);
-	},
-
-	target: function() {
-		// Return the local target.
-		return UIATarget.localTarget();
-	},
-
-	delay: function(seconds) {
-		// Delay the target by `seconds` (can be a fraction)
-		// Defaults to setting in configuration
-		seconds = seconds || this.config.delayBetweenEvents;
-		this.target().delay(seconds);
-	},
-
-	chooseEventName: function() {
-		// Randomly chooses an event name from the `eventsWeight` dictionary
-		// based on the given weights.
-		var calculatedEventWeights = [];
-		var totalWeight = 0;
-		var events = this.config.eventWeights;
-		for (var event in events) {
-			if (events.hasOwnProperty(event)) {
-				calculatedEventWeights.push({
-					weight: events[event]+totalWeight,
-					event: event
-				});
-				totalWeight += events[event];
-			}
-		}
-
-		var chosenWeight = Math.random() * 1000 % totalWeight;
-
-		for (var i = 0; i < calculatedEventWeights.length; i++) {
-			if (chosenWeight < calculatedEventWeights[i].weight) {
-				return calculatedEventWeights[i].event;
-			}
-		}
-
-		throw "No even was chosen!";
-	},
-
-	screenWidth: function() {
-		// Need to adjust by one to stay within rectangle
-		return this.target().rect().size.width - 1;
-	},
-
-	screenHeight: function() {
-		// Need to adjust by one to stay within rectangle
-		return this.target().rect().size.height - 1;
-	},
-
-	randomX: function() {
-		var min, max;
-
-		if (this.config.frame){
-			// Limits coordinates to given frame if set in config
-			min = this.config.frame.origin.x;
-			max = this.config.frame.size.width + min;
-		} else {
-			// Returns a random X coordinate within the screen rectangle
-			min = 0;
-			max = this.screenWidth();
-		}
-
-		return Math.floor(Math.random() * (max - min) + min) + 1;
-	},
-
-	randomY: function() {
-		var min, max;
-
-		if (this.config.frame){
-			// Limits coordinates to given frame if set in config
-			min = this.config.frame.origin.y;
-			max = this.config.frame.size.height + min;
-		} else {
-			// Returns a random Y coordinate within the screen rectangle
-			min = 0;
-			max = this.screenHeight();
-		}
-
-		return Math.floor(Math.random() * (max - min) + min) + 1;
-	},
-
-	randomTapCount: function() {
-		// Calculates a tap count for tap events based on touch probabilities
-		if (this.config.touchProbability.multipleTaps > Math.random()) {
-			return Math.floor(Math.random() * 10) % 3 + 1;
-		}
-		else return 1;
-	},
-
-	randomTouchCount: function() {
-		// Calculates a touch count for tap events based on touch probabilities
-		if (this.config.touchProbability.multipleTouches > Math.random()) {
-			return Math.floor(Math.random() * 10) % 3 + 1;
-		}
-		else return 1;
-	},
-
-	randomTapDuration: function() {
-		// Calculates whether or not a tap should be a long press based on
-		// touch probabilities
-		if (this.config.touchProbability.longPress > Math.random()) {
-			return 0.5;
-		}
-		else return 0;
-	},
-
-	randomRadians: function() {
-		// Returns a random radian value
-		return Math.random() * 10 % (3.14159 * 2);
-	},
-
-	takeScreenShotIfItIsTime: function() {
-		var now = (new Date()).valueOf();
-		if (!this._lastScreenshotTime) this._lastScreenshotTime = 0;
-
-		if (now - this._lastScreenshotTime > this.config.screenshotInterval * 1000) {
-			var filename = "monkey-" + (new Date()).toISOString().replace(/[:\.]+/g, "-");
-			this.target().captureScreenWithName(filename);
-			this._lastScreenshotTime = now;
-		}
 	}
 };
 
-UIAutoMonkey.RELEASE_THE_MONKEY();
+// --- --- --- ---
+// Helper methods
+//
 
-/* Instruments uses 4-wide tab stops. */
-/* vim: set tabstop=4 shiftwidth=4 softtabstop=4 copyindent noexpandtab: */
+UIAutoMonkey.prototype.RELEASE_THE_MONKEY = function() {
+	// Called at the bottom of this script to, you know...
+	//
+	// RELEASE THE MONKEY!
+
+	for(var i = 0; i < this.config.numberOfEvents; i++) {
+		this.triggerRandomEvent();
+		if (this.config.screenshotInterval) this.takeScreenShotIfItIsTime();
+		this.delay();
+	}
+};
+
+UIAutoMonkey.prototype.triggerRandomEvent = function() {
+	var name = this.chooseEventName();
+	// Find the event method based on the name of the event
+	var event = this.allEvents[name];
+	event.apply(this);
+};
+
+UIAutoMonkey.prototype.target = function() {
+	// Return the local target.
+	return UIATarget.localTarget();
+};
+
+UIAutoMonkey.prototype.delay = function(seconds) {
+	// Delay the target by `seconds` (can be a fraction)
+	// Defaults to setting in configuration
+	seconds = seconds || this.config.delayBetweenEvents;
+	this.target().delay(seconds);
+};
+
+UIAutoMonkey.prototype.chooseEventName = function() {
+	// Randomly chooses an event name from the `eventsWeight` dictionary
+	// based on the given weights.
+	var calculatedEventWeights = [];
+	var totalWeight = 0;
+	var events = this.config.eventWeights;
+	for (var event in events) {
+		if (events.hasOwnProperty(event)) {
+			calculatedEventWeights.push({
+				weight: events[event]+totalWeight,
+				event: event
+			});
+			totalWeight += events[event];
+		}
+	}
+
+	var chosenWeight = Math.random() * 1000 % totalWeight;
+
+	for (var i = 0; i < calculatedEventWeights.length; i++) {
+		if (chosenWeight < calculatedEventWeights[i].weight) {
+			return calculatedEventWeights[i].event;
+		}
+	}
+
+	throw "No even was chosen!";
+};
+
+UIAutoMonkey.prototype.screenWidth = function() {
+	// Need to adjust by one to stay within rectangle
+	return this.target().rect().size.width - 1;
+};
+
+UIAutoMonkey.prototype.screenHeight = function() {
+	// Need to adjust by one to stay within rectangle
+	return this.target().rect().size.height - 1;
+};
+
+UIAutoMonkey.prototype.randomX = function() {
+	var min, max;	
+
+	if (this.config.frame){
+		// Limits coordinates to given frame if set in config
+		min = this.config.frame.origin.x;
+		max = this.config.frame.size.width + min;
+	} else {
+		// Returns a random X coordinate within the screen rectangle
+		min = 0;
+		max = this.screenWidth();
+	}
+
+	return Math.floor(Math.random() * (max - min) + min) + 1;
+};
+
+UIAutoMonkey.prototype.randomY = function() {
+	var min, max;
+
+	if (this.config.frame){
+		// Limits coordinates to given frame if set in config
+		min = this.config.frame.origin.y;
+		max = this.config.frame.size.height + min;
+	} else {
+		// Returns a random Y coordinate within the screen rectangle
+		min = 0;
+		max = this.screenHeight();
+	}
+
+	return Math.floor(Math.random() * (max - min) + min) + 1;
+};
+
+UIAutoMonkey.prototype.randomTapCount = function() {
+	// Calculates a tap count for tap events based on touch probabilities
+	if (this.config.touchProbability.multipleTaps > Math.random()) {
+		return Math.floor(Math.random() * 10) % 3 + 1;
+	}
+	else return 1;
+};
+
+UIAutoMonkey.prototype.randomTouchCount = function() {
+	// Calculates a touch count for tap events based on touch probabilities
+	if (this.config.touchProbability.multipleTouches > Math.random()) {
+		return Math.floor(Math.random() * 10) % 3 + 1;
+	}
+	else return 1;
+};
+
+UIAutoMonkey.prototype.randomTapDuration = function() {
+	// Calculates whether or not a tap should be a long press based on
+	// touch probabilities
+	if (this.config.touchProbability.longPress > Math.random()) {
+		return 0.5;
+	}
+	else return 0;
+};
+
+UIAutoMonkey.prototype.randomRadians = function() {
+	// Returns a random radian value
+	return Math.random() * 10 % (3.14159 * 2);
+};
+
+UIAutoMonkey.prototype.takeScreenShotIfItIsTime = function() {
+	var now = (new Date()).valueOf();
+	if (!this._lastScreenshotTime) this._lastScreenshotTime = 0;
+
+	if (now - this._lastScreenshotTime > this.config.screenshotInterval * 1000) {
+		var filename = "monkey-" + (new Date()).toISOString().replace(/[:\.]+/g, "-");
+		this.target().captureScreenWithName(filename);
+		this._lastScreenshotTime = now;
+	}
+};
+
+// Commodity function to call RELEASE_THE_MONKEY directly on UIAutoMonkey
+// if you don't need to customize your instance
+UIAutoMonkey.RELEASE_THE_MONKEY = function() {
+	(new UIAutoMonkey()).RELEASE_THE_MONKEY();
+};
+
+// UIAutoMonkey.RELEASE_THE_MONKEY();
+
+
+
+
+// --- --- --- ---
+
+// Usage & Customization example
+// Save this UIAutoMonkey.js somewhere in your disk to import it and configure it in each of your Instruments instances
+/*
+#import "UIAutoMonkey.js"
+
+// Configure the monkey: use the default configuration but a bit tweaked
+monkey = new UIAutoMonkey()
+monkey.config.numberOfEvents = 1000;
+monkey.config.screenshotInterval = 5;
+// Of course, you can also override the default config completely with your own, using `monkey.config = { … }` instead
+
+// Configure some custom events if needed
+monkey.config.eventWeights.customEvent1 = 300;
+monkey.allEvents.customEvent1 = function() { … }
+
+// Release the monkey!
+monkey.RELEASE_THE_MONKEY();
+*/
