aef5dabbc6ddfd866e252be0ad0acc5de1817ec0
Bruce Gordon <bruceg@yahoo-inc.com>
2015-06-19 13:05:03 -0400

    wip

---
+++ SampleCustomization/Includes.js
@@ -0,0 +1,3 @@
+#import "SetGlobals.js"
+#import "../ui-auto-monkey/buttonHandler.js"
+#import "../ui-auto-monkey/UIAutoMonkey.js"
---
+++ SampleCustomization/SampleTest.js
@@ -0,0 +1,6 @@
+"use strict";
+#import "Includes.js"
+
+var monkey = new UIAutoMonkey();
+monkey.config.minutesToRun = 2;
+monkey.RELEASE_THE_MONKEY();
